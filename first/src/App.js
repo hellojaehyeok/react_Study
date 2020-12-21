04959a1de8bad577bf1c7b25245bf4eaa6dff5e5
Song jae hyeok <wakij6587@naver.com>
2020-12-21 12:10:44 +0900

    react 설치 및 기초 공부
    react 설치 및 기초 공부

---
+++ first/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import Counter from './counter';
+
+function App() {
+
+  return (
+    
+    <div className="App">
+      <h1>Song Jae Hyeok</h1>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ first/src/counter.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+const Counter = ()=>{
+    const [count, setCount] = useState(0);
+    const increment = () => {
+      setCount(count + 1);
+    }
+    return (
+        <button onClick={increment}>
+            Click {count}
+        </button>
+    );
+};
+
+export default Counter; 
