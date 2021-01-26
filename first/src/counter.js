7cfb67e99de5afdf156201fdc57a5cad613d4742
Song jae hyeok <wakij6587@naver.com>
2021-01-26 17:39:41 +0900

    props 추가
    props 추가

--- first/src/counter.js
+++ first/src/counter.js
@@ -1,13 +1,16 @@
 import React, {useState} from 'react';
 
-const Counter = ()=>{
+const Counter = (props)=>{
     const [count, setCount] = useState(0);
     const increment = () => {
       setCount(count + 1);
     }
+
+    const clickString = props.click || 'Click'
+    
     return (
         <button onClick={increment}>
-            Click {count}
+            {clickString}{count}
         </button>
     );
 };
