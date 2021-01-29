f16e2fa398f9095fafa461349c8f612f37a31ddc
Song jae hyeok <wakij6587@naver.com>
2021-01-29 15:32:34 +0900

    todolist 스타일 및 카운트 증가, 감소 이벤트 생성
    todolist 스타일 및 카운트 증가, 감소 이벤트 생성

--- practice/src/components/todo.jsx
+++ practice/src/components/todo.jsx
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+
+    state = {
+        count : 0
+    };
+
+    handleIncrement = () =>{
+        this.setState({ count : this.state.count + 1});
+    };
+    
+    handleDecrement = () =>{
+        const count = this.state.count - 1;
+        this.setState({ count : count < 0 ? 0 : count});
+    };
+
     render() {
         return (
-            <>
+            <li className="todo">
                 <span className="todo-name">Coding</span>
-                <span className="todo-count">0</span>
-                <button className="todo-button todo-increase">
+                <span className="todo-count">{this.state.count}</span>
+                <button className="todo-button todo-increase"  onClick={this.handleIncrement}>
                     <i className="fas fa-plus"></i>
                 </button>
-                <button className="todo-button todo-decrease">
+                <button className="todo-button todo-decrease" onClick={this.handleDecrement}>
                     <i className="fas fa-minus"></i>
                 </button>
                 <button className="todo-button todo-delete">
                     <i className="fas fa-trash"></i>
                 </button>
-            </>
+            </li>
         );
     }
 }
