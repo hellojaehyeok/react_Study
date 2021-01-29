6917480b630d29577ddaa054af1a486396b2d65d
Song jae hyeok <wakij6587@naver.com>
2021-01-29 18:22:49 +0900

    nav 생성, total count 이벤트 생성
    nav 생성, total count 이벤트 생성

--- practice/src/app.jsx
+++ practice/src/app.jsx
@@ -1,14 +1,65 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import './app.css';
-import Todo from './components/todo';
 import Todos from './components/todos';
+import Nav from './components/nav';
 
-function App() {
-  return 
-    <>
-      
-      <Todos />;
-    </>
+class App extends Component {
+
+  state = {
+    todos:[
+        { id : 1, name : "React", count : 0},
+        { id : 2, name : "Three.js", count : 0},
+        { id : 3, name : "jQuery", count : 0},
+    ],count : 0
+  };
+
+  handleIncrement = (todo) =>{
+      const todos = [...this.state.todos];
+      const index = todos.indexOf(todo);
+      todos[index].count++;
+      const navCount = this.state.count+1;
+      this.setState({todos, count : navCount});
+  };
+
+  handleDecrement = (todo) =>{
+      const todos = [...this.state.todos];
+      const index = todos.indexOf(todo);
+      const count = todos[index].count - 1;
+      todos[index].count = count < 0 ? 0 : count;
+      let navCount = this.state.count;
+      if(count >= 0){
+        navCount -= 1;
+      }
+      this.setState({todos, count : navCount});
+  };
+
+  handleDelete = (todo) =>{
+    let todos = [...this.state.todos];
+    const index = todos.indexOf(todo);
+    const todoCount = todos[index].count;
+    let navCount = this.state.count;
+    navCount -= todoCount;
+    
+    todos = this.state.todos.filter(item =>item.id !== todo.id)
+    this.setState({todos, count : navCount});
+  };
+
+  render() {
+    return (
+      <>
+        <Nav 
+          totalCount={this.state.count}
+        />
+        
+        <Todos 
+          todos={this.state.todos}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
+  };
 }
 
 export default App;
--- practice/src/components/todos.jsx
+++ practice/src/components/todos.jsx
@@ -2,40 +2,25 @@ import React, { Component } from 'react';
 import Todo from './todo';
 
 class Todos extends Component {
-    state = {
-        todos:[
-            { id : 1, name : "React", count : 0},
-            { id : 2, name : "Three.js", count : 0},
-            { id : 3, name : "jQuery", count : 0},
-        ],
-    };
 
     handleIncrement = (todo) =>{
-        const todos = [...this.state.todos];
-        const index = todos.indexOf(todo);
-        todos[index].count++;
-        this.setState({todos});
+        this.props.onIncrement(todo);
     };
     
     handleDecrement = (todo) =>{
-        const todos = [...this.state.todos];
-        const index = todos.indexOf(todo);
-        const count = todos[index].count - 1;
-        todos[index].count = count < 0 ? 0 : count;
-        this.setState({todos});
+        this.props.onDecrement(todo);
     };
 
     handleDelete = (todo) =>{
-        const todos = this.state.todos.filter(item =>item.id !== todo.id)
-        this.setState({todos});
+        this.props.onDelete(todo);
     };
 
 
     render() {
         return (
-            <ul>
+            <ul className="list-wrap">
                 {
-                    this.state.todos.map(todo =>(
+                    this.props.todos.map(todo =>(
                         <Todo
                         key={todo.id}
                         todo={todo}
